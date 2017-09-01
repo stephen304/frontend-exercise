4bab44c0f3351602076a20adfe8e8ffd403accac
Tom Clegg <tclegg@veritasgenetics.com>
2017-08-31 23:18:17 -0400

    Starting point.

---
+++ app.js
@@ -0,0 +1,31 @@
+function Thinger() {
+    var thinger = this
+    thinger.count = 0
+    thinger.times = []
+    thinger.ping = function() {
+        thinger.count++
+        thinger.times.push(new Date())
+    }
+}
+
+var thinger = new Thinger()
+
+var Page = {
+    view: function(vnode) {
+        return m('div', [
+            m('p', 'Here is a button.'),
+            m('button', {
+                onclick: thinger.ping,
+            }, 'click me!'),
+            m('p', 'You have clicked ', thinger.count, ' times. Try one more.'),
+            m('ul', [
+                'Click times:',
+                thinger.times.map(function(t) {
+                    return m('li', t.toString())
+                }),
+            ]),
+        ])
+    },
+}
+
+m.mount(document.body, Page)
