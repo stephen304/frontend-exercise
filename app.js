4e9d086fe345b8ba7d9058e55414494d1bfb6d6b
Stephen Smith <stephen304@gmail.com>
2019-10-23 19:42:28 -0400

    feat: Change click time to top 5 fastest double clicks

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 function Thinger() {
   var thinger = this
   thinger.count = 0
-  thinger.times = []
+  thinger.bestTimes = []
+  thinger.lastTime = false
   thinger.ping = function() {
+    // Get the time of this click
+    let currentTime = new Date()
+
+    // If there is a previous click, calculate the delta
+    if (thinger.lastTime) {
+      let timeDelta = currentTime - thinger.lastTime
+      // Store the new delta
+      thinger.bestTimes.push(timeDelta)
+      // Sort the best times
+      thinger.bestTimes.sort(function(a, b) {
+        return a - b
+      })
+      // Truncate best times to the top 5
+      thinger.bestTimes = thinger.bestTimes.slice(0, 5)
+    }
+
+    // Store the current click time as last time for the next click
+    thinger.lastTime = currentTime
     thinger.count++
-    thinger.times.push(new Date())
   }
 }
 
@@ -16,15 +34,15 @@ var Page = {
       m('p', 'Here is a button.'),
       m('button', {
         onclick: thinger.ping,
-      }, 'click me!'),
+      }, 'Click me!'),
       m('p', 'You have clicked ', thinger.count, ' times. Try one more.'),
       m('ul', [
-        'Click times:',
-        thinger.times.map(function(t) {
+        'Top 5 Double Click Intervals:',
+        thinger.bestTimes.map(function(t) {
           return m('li', [
             m('span', {
               class: 'clicktime'
-            }, t.toString())
+            }, [t.toString(), ' milliseconds'])
           ])
         }),
       ]),
