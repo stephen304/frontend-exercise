dde45ae84836e281ffcad4b1869b5b63c60287d3
Stephen Smith <stephen304@gmail.com>
2019-10-23 22:18:03 -0400

    feat: Add threshold for double click timing, only count double clicks in counter

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ function DoubleClickDetector() {
   // Decode previously stored data, falsy if nonexistant
   storedData = storedData && JSON.parse(storedData)
 
+  // Double click threshold setting
+  const THRESHOLD = 500
+
   // Merge previous data over default values
   dcd.data = Object.assign({
     count: 0,
@@ -13,25 +16,32 @@ function DoubleClickDetector() {
   dcd.lastTime = false
   dcd.ping = function() {
     // Get the time of this click
-    let currentTime = new Date()
-
+    var currentTime = new Date()
     // If there is a previous click, calculate the delta
     if (dcd.lastTime) {
       let timeDelta = currentTime - dcd.lastTime
-      // Store the new delta
-      dcd.data.bestTimes.push(timeDelta)
-      // Sort the best times
-      dcd.data.bestTimes.sort(function(a, b) {
-        return a - b
-      })
-      // Truncate best times to the top 5
-      dcd.data.bestTimes = dcd.data.bestTimes.slice(0, 5)
+      // Only count double clicks that are fast enough
+      if (timeDelta < THRESHOLD) {
+        // Store the new delta
+        dcd.data.bestTimes.push(timeDelta)
+        // Sort the best times
+        dcd.data.bestTimes.sort(function(a, b) {
+          return a - b
+        })
+        // Truncate best times to the top 5
+        dcd.data.bestTimes = dcd.data.bestTimes.slice(0, 5)
+        // Increment count of double clicks
+        dcd.data.count++
+        // Clear lastTime to not count the final click as the start of the next double click
+        dcd.lastTime = false
+      } else {
+        // If threshold was not met, count this click as the first click
+        dcd.lastTime = currentTime
+      }
+    } else {
+      // Store the current click time as last time for the next click
+      dcd.lastTime = currentTime
     }
-
-    // Store the current click time as last time for the next click
-    dcd.lastTime = currentTime
-    dcd.data.count++
-
     // Store updated data in localstorage
     localStorage.setItem('clickDetectorData', JSON.stringify(dcd.data));
   }
@@ -60,14 +70,14 @@ var Page = {
         m('p', [
           m('a[href="#"].btn.btn-primary.ping', {
             onclick: dcd.ping,
-          }, 'Click me!'),
+          }, 'Double click me!'),
           ' ',
           m('a[href="#"].btn.btn-secondary.clear', {
             onclick: dcd.clear,
           }, 'Clear best times'),
         ]),
         m('p', [
-          'You have clicked ', dcd.data.count, ' times. Try one more.',
+          'You have double clicked ', dcd.data.count, ' times. Try one more.',
           m('br'),
           m('h3', 'Double Click High Scores'),
           m('div.table-responsive', m('table.table.table-striped.table-sm', [
